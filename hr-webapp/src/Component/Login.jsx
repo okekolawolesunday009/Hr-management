3d4fc637407c17391022303a8acaab70a9789247
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-12 08:27:46 +0100

    links

--- hr-webapp/src/Component/Login.jsx
+++ hr-webapp/src/Component/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
     const navigate = useNavigate()
     const handleSubmit = (e) =>{
         e.preventDefault()
-        axios.post('http://localhost:8080/login', formData)
+        axios.post('https://hr-management-kvqb.onrender.com/login', formData)
         .then(res => {
           if(res.data.Status === 'success'){
              navigate('/home')
