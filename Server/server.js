309edb9ff7a9b9da781cb6f89df229c3b96297af
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-08 11:35:07 +0100

    image apicors

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ import { waitForDebugger } from "inspector";
 
 
 const app = express()
-// app.use(cors())
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('public'))
