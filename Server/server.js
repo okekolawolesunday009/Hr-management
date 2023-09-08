e9e3fd866cdba00951f4f2e3c5ddefc4c373b8b8
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-08 12:04:20 +0100

    remove toasts remove cors

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mysql from 'mysql'
-import cors from 'cors'
+// import cors from 'cors'
 import cookieParser from "cookie-parser";
 import bcrypt from 'bcrypt'
 import  Jwt  from "jsonwebtoken";
@@ -11,7 +11,7 @@ import { waitForDebugger } from "inspector";
 
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('public'))
